dd3216bcc993e454616dff5e9d020371576d56a7
yuanjun <imoowi@qq.com>
2023-06-21 09:54:31 +0800

    v1.1.2

--- comer/comer.tips.go
+++ comer/comer.tips.go
@@ -3,5 +3,8 @@ package comer
 import "fmt"
 
 func (c *Comer) showTips() {
-	fmt.Printf("接下来，请执行以下操作:\n1、修改配置文件（%s/configs/settings-local.yml）数据库和redis的配置\n2、cd %s \n3、air\n", c.path, c.path)
+	fmt.Println(`接下来，请执行以下操作:`)
+	fmt.Println(`1、修改配置文件（` + c.path + `/configs/settings-local.yml）数据库和redis的配置`)
+	fmt.Println(`2、cd  ` + c.path)
+	fmt.Println(`3、air 或者 swag init && go mod tidy && go run .`)
 }
