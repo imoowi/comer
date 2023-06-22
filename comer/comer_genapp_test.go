af05692bbc7facb8be47ffa662f0569c39237bc1
袁君 <simpleyuan@gmail.com>
2023-06-22 22:15:46 +0800

    add test files

---
+++ comer/comer_genapp_test.go
@@ -0,0 +1,21 @@
+package comer
+
+import "testing"
+
+func TestGenApp(t *testing.T) {
+
+}
+func TestGenerateAppDir(t *testing.T) {
+}
+func TestGenerateAppFiles(t *testing.T) {
+
+}
+func TestShowAppTips(t *testing.T) {
+
+}
+func TestAddAppsDepend(t *testing.T) {
+
+}
+func TestAddAppRouterDepend(t *testing.T) {
+
+}
--- main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestMain(t *testing.T) {
-	if 1 == 2 {
-		t.Errorf(`main test failed`)
-	}
-}
