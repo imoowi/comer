847a8271f33567b0e97aabdea3ba09326f652ed9
yuanjun <imoowi@qq.com>
2023-06-26 14:21:43 +0800

    add unit test

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -12,15 +12,16 @@ func TestRoot(t *testing.T) {
 	output, _ := executeCommand(rootCmd, "--module", "user", "--path", "tmp/comer-example")
 	fmt.Println(output)
 }
+
 func TestVersion(t *testing.T) {
 	output, _ := executeCommand(rootCmd, "version")
 	fmt.Println(output)
 }
+
 func TestGenapp(t *testing.T) {
 	rootCmd.AddCommand(genappCmd)
 	output, _ := executeCommand(rootCmd, "genapp", "--app", "user")
-	fmt.Println(output)
-	// assert.Equal(t, "test", output)
+	fmt.Println(`output is : `, output)
 }
 
 func executeCommand(root *cobra.Command, args ...string) (output string, err error) {
