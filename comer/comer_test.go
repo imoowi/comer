036dddcd99c7da3d2a62353b3c4b5215ee3e8b10
yuanjun <imoowi@qq.com>
2023-06-26 13:16:19 +0800

    update unit test

--- comer/comer_test.go
+++ comer/comer_test.go
@@ -13,4 +13,17 @@ func TestComer(t *testing.T) {
 	if comerIns.showTips() != `showTips called` {
 		t.Errorf(`showTips test failed`)
 	}
+	comerIns.Start(nil, nil)
+	comerIns.generateFrameworkDir()
+	comerIns.generateFrameworkFiles()
+	comerIns.generateAppDir()
+	comerIns.generateAppFiles()
+	comerIns.showAppTips()
+	comerIns.addAppsDepend()
+	comerIns.addAppRouterDepend()
+	comerIns.initApp(nil, nil)
+	comerIns.init(nil, nil)
+	comerIns.generateDirByName(`testdir`)
+	comerIns.generateFileByMap(`abc`, `a`, `d`)
+	comerIns.showLogo()
 }
