53e8caf869c9ea8157cf765231b2c094cc3745cb
coding-jun <simpleyuan@gmail.com>
2026-01-26 10:21:11 +0800

    fix(tpl):fix interfaces and tpl

--- comer/comer.version.go
+++ comer/comer.version.go
@@ -13,7 +13,7 @@ func (c *Comer) goVersion() string {
 }
 
 func (c *Comer) Version() string {
-	c.version = `v1.3.10`
+	c.version = `v1.3.11`
 	fmt.Println(`Comer version `, c.version)
 	return c.version
 }
